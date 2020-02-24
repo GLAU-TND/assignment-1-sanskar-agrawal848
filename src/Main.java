fa7ad6708afa815ef3d1933244976ce6fea7013b
Sanskar <sanskar.agrawal_ccv18@gla.ac.in>
2020-02-24 22:23:21 +0530

    Creating Main class for implementation.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Contacts c = new Contacts();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to DBC's Contact List App");
+        System.out.println("Press 1 to add a new contact");
+        System.out.println("Press 2 to view all contacts");
+        System.out.println("Press 3 to search for a contact");
+        System.out.println("Press 4 to delete a contact");
+        System.out.println("Press 5 to exit program" + "\n");
+    }
